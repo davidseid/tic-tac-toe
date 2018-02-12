3a6f8e8abbc4175fd9394be9e6b0355600370e15
David Seidenberg <davidseidenberg@gmail.com>
2018-02-12 11:39:21 -0800

    Add basic CSS styling to create game table

---
+++ app.js
@@ -0,0 +1,12 @@
+// My Tic Tac Toe App
+
+// Should be a source of truth for the tic tac toe game
+// basically an array of arrays 
+// each time there is a change, the app should update the model and rerender everything
+
+// information should be drawn with divs that hold nothing or an x
+
+// first step, create an html page with a blank tic tac toe board 
+// get that to render
+
+console.log('hi');
