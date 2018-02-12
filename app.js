9b04da539faf29e48685ebe883f6c6aef125e5f9
David Seidenberg <davidseidenberg@gmail.com>
2018-02-12 12:30:17 -0800

    Get divs to listen to the user clicks and display X on DOM
    Need to get the model to update along with it

--- app.js
+++ app.js
@@ -4,9 +4,45 @@
 // basically an array of arrays 
 // each time there is a change, the app should update the model and rerender everything
 
-// information should be drawn with divs that hold nothing or an x
+// FUNCITONALITY
+// When page opens, allow user to click on a div to place an x in there
+// computer responds with an 0 in a random remaining spot
+// game then waits for the user to put a new x in there
+// when one of the players wins or ties - print out a result to the page 
+// when new game is hit, the everything resets to blank 
 
-// first step, create an html page with a blank tic tac toe board 
-// get that to render
+// make an array of arrays to hold all the information
 
-console.log('hi');
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log('DOM loaded')
+
+  var board = [];
+
+  for (var i = 0; i < 3; i++) {
+    var row = ['','','']
+    board.push(row);
+  }
+
+  // need a helper function that takes the class name of a cell
+  // identifies the correct cell on the board
+  // and adds an x in there
+
+  var addXToModel = function(classList) {
+    
+  }
+
+  // Click listener to add x's 
+  var cells = document.getElementsByClassName('cell');
+
+  for (var i = 0; i < cells.length; i++) {
+    var cell = cells[i];
+    cell.addEventListener('click', function(event) {
+      event.target.textContent = 'X';
+      console.log(event.target.classList);
+
+    });
+  }
+
+});
