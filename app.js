872b121c692cb454233a320f1e27f87ebfc68e54
David Seidenberg <davidseidenberg@gmail.com>
2018-02-12 16:33:01 -0800

    Improve new game button

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 // My Tic Tac Toe App
 
-// Should be a source of truth for the tic tac toe game
-// basically an array of arrays 
-// each time there is a change, the app should update the model and rerender everything
-
-// FUNCITONALITY
-// When page opens, allow user to click on a div to place an x in there
-// computer responds with an 0 in a random remaining spot
-// game then waits for the user to put a new x in there
-// when one of the players wins or ties - print out a result to the page 
-// when new game is hit, the everything resets to blank 
-
-// make an array of arrays to hold all the information
+// REFACTOR GOALS
+  // Make the model the one source of truth and display to the DOM based on that
+  // Improve column and diagonal checking
+  // DRY -- get rid of repeat code
+  // Style it up so that it is fun when you win
+  // make the new game button more prominent
+  // Refactor layout with flexbox
 
 
 
